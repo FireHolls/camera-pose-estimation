9c96f5c38613252272cf6e6eb9aa1432f4a8b0d1
Thien Dao <thiendaodinhddt@gmail.com>
2026-04-27 22:30:09 +0200

    update

--- eight_points/testing/Test_RANSAC.py
+++ eight_points/testing/Test_RANSAC.py
@@ -14,6 +14,36 @@ from plot_fct import plot_points
 import matplotlib.pyplot as plt
 from eight_points.RANSAC import RANSAC
 
+def score_F(F, px1, px2, threshold=3.84):
+    """
+    Symmetric Sampson distance score for fundamental matrix F (ORB-SLAM style).
+
+    Sampson distance is the first-order approximation of the reprojection error
+    under the epipolar constraint x2ᵀFx1 = 0 :
+
+      d²_sampson = (x2ᵀFx1)² / (‖Fx1‖²_top2 + ‖Fᵀx2‖²_top2)
+
+      S_F = Σ max(0, T - d²_sampson)
+
+    threshold : chi² at 95% for 1 DOF = 3.84  (epipolar constraint is 1D)
+
+    Returns: float  (higher = better fit)
+    """
+    N = px1.shape[1]
+    h1 = np.vstack([px1, np.ones((1, N))])
+    h2 = np.vstack([px2, np.ones((1, N))])
+
+    Fx1  = F @ h1                                              # (3, N)
+    Ftx2 = F.T @ h2                                           # (3, N)
+
+    num      = np.sum(h2 * Fx1, axis=0) ** 2                  # (x2ᵀFx1)²
+    denom    = Fx1[0]**2 + Fx1[1]**2 + Ftx2[0]**2 + Ftx2[1]**2
+    d_samp   = num / (denom + 1e-12)
+    score = float(np.sum(np.maximum(0, threshold - d_samp)))
+    inlier_mask = d_samp < threshold
+    return score, inlier_mask
+
+
 # 1 Génération de la scène 
 bounds = np.array([-2.0, 2.0, -1.5, 1.5, 10, 20])
 pts3d  = PointsGenerator(nbPoints=50, seed=43, bounds=bounds)
@@ -46,11 +76,12 @@ px2_vis = px2[:, vis]
 ransac_solver = RANSAC(
     s=8, 
     epsilon=0.2, 
-    eight_points=eight_point, 
+    score_fct=score_F,
+    model_fct=eight_point, 
     px1=px1_vis, 
     px2=px2_vis
 )
-F, mask = ransac_solver.execute_RANSAC_F()
+F, mask = ransac_solver.execute_RANSAC()
 
 clean_px1 = px1_vis[:, mask]
 clean_px2 = px2_vis[:, mask]
